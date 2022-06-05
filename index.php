e3c6f985b690831a9ce97cde81769b3710341e55
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-06-05 23:15:42 +0900

    commitResult

---
+++ index.php
@@ -0,0 +1,250 @@
+﻿<!DOCTYPE HTML>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>ULSAN HYUNDAI FC FAN SITE</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									<a href="index.html" class="logo"><strong>Editorial</strong> by HTML5 UP</a>
+									<ul class="icons">
+										<li><a href="https://twitter.com/ulsanfc" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="https://ko-kr.facebook.com/ulsanfc/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+										<li><a href="#" class="icon brands fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
+										<li><a href="https://www.instagram.com/ulsanhyundaifootballclub/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="register.html" class="icon solid arrow-right-to-bracket-solid"><span class="label">Register</span></a></li>
+									</ul>
+								</header>
+
+							<!-- Banner -->
+								<section id="banner">
+									<div class="content">
+										<header>
+											<h1>ULSAN HYUNDAI FC<br />
+											울산 현대 축구단</h1>
+											<p>한 대학생의 과제 하는 김에 축구단 홍보하는 사이트</p>
+										</header>
+										<p>과제로 CRUD가 충족하는 사이트를 만들어야하는데, 소재로 생각나는 것이 없어서 내가 좋아하는 축구팀이나 홍보하자는 생각이 들어서 울산 현대에 대해 소개하며 간단하게 CRUD개념을 추가하기위해 게시판 기능까지 넣어보려한다.</p>
+										<ul class="actions">
+											<li><a href="https://www.uhfc.tv/" class="button big">공식 홈페이지</a></li>
+										</ul>
+									</div>
+									<span class="image object">
+										<img src="images/ULSAN_HYUNDAI.png" alt="이미지가 가출했습니다" />
+									</span>
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>울산 현대 축구단</h2>
+									</header>
+									<div class="features">
+										<article>
+											<span class="icon solid trophy-solid"></span>
+											<div class="content">
+												<h3>간단한 역사</h3>
+												<p>1. 1984년 창단<br>
+													2. 1996년 우승<br>
+													3. 2005년 우승<br>
+													<strong>4. 2012년 ACL 무패우승</strong><br>
+													<strong>5. 2020년 ACL 무패우승</strong>
+												</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon solid fa-paper-plane"></span>
+											<div class="content">
+												<h3>라이벌리</h3>
+												<p><strong>동해안 더비: vs 포항 스틸러스</strong><br>
+													K리그 동남부의 결투(The K League's south-east scrap, FIFA 소개문) FIFA인증 가장 오래된 대한민국 축구 라이벌, 김병지의 헤딩골이 1998년 포항과의 경기였다<br>
+													<strong>현대가 더비: vs 전북 현대 모터스</strong><br>
+													K리그에서 현재 가장 치열한 라이벌, 사실상 이 두팀의 경기는 국가대표간의 경기와 같다
+												</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon solid fa-solid fa-comments"></span>
+											<div class="content">
+												<h3>팬 사이트 : ULTIMATE</h3>
+												<p>
+													울산 현대의 팬들이 모여 소통하는 팬 커뮤니티, <strong>울산 및 전국 원정 경기 지역의 맛집 소개</strong> 및 축구 소식을 같이 볼 수 있다.
+												</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon solid fa-solid paw-solid"></span>
+											<div class="content">
+												<h3>각종 컨텐츠</h3>
+												<p><strong>유튜브</strong><br>
+													베리나히쑤<br>
+													UHFC TV
+												</p>												
+											</div>
+										</article>
+									</div>
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>울산 더 알아보기</h2>
+									</header>
+									<div class="posts">
+										<article>
+											<a href="#" class="image"><img src="images/seol.jpg" alt="사진이 가출했습니다" /></a>
+											<h3>울산 아이돌: 설영우</h3>
+											<p>일명 울산 박형식, 울산의 아이돌 그 자체이며, 축구 실력또한 출중하다. 개인적인 의견으로는 다음 국대 주전 풀백은 설영우 선수가 될 것이다.</p>
+											<ul class="actions">
+												<li><a href="https://www.google.com/search?q=%EC%84%A4%EC%98%81%EC%9A%B0&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi23czarJT4AhVVCd4KHYATCC0Q_AUoAXoECAIQAw&biw=1920&bih=937&dpr=1" class="button">시청하기</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/Very.png" alt="사진이 가출했습니다." /></a>
+											<h3>베리나히쑤: Asia No.1 조수혁 골키퍼</h3>
+											<p>2020년 ACL의 무패우승을 이끈 ASIA의 넘버원 골키퍼 조수혁 선수가 운영하는 울산 현대 선수단의 V-Log 울산 현대의 뒷이야기 혹은 선수 개인을 보고 싶다면 이것보다 좋은 선택은 없다</p>
+											<ul class="actions">
+												<li><a href="https://www.youtube.com/channel/UCpyJI8Oi6Ylol3Y2sb519NA" class="button">시청하기</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/BlueWave.jpg" alt="" /></a>
+											<h3>푸른 파도: 다큐멘터리</h3>
+											<p>K리그 팀이라는 어찌보면 마이너한 컨텐츠를 아주 흥미롭게 다루는 컨텐츠, 2편까지 나왔다</p>
+											<ul class="actions">
+												<li><a href="https://www.youtube.com/watch?v=PfnVUQ3OZgQ&list=PL4DwivM7zDmoycq9XT11eAcA0kbq67BDe&ab_channel=%EC%9A%B8%EC%82%B0%ED%98%84%EB%8C%80%EC%B6%95%EA%B5%AC%EB%8B%A8-UlsanHyundaiFootballClub" class="button">시청하기</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/ULSAN_MITA.jpg" alt="" /></a>
+											<h3>울산 현대 마스코트 MITA</h3>
+											<p>울산 현대의 마스코트, 현재 K리그 마스코트 부반장이며 구단 공식 미친타이거 이다. 저번엔 주식에 미친 고양이 컨셉을 잡았다</p>
+											<ul class="actions">
+												<li><a href="#" class="button">더 보여드릴게 없습니다</a></li>
+											</ul>
+										</article>
+									</div>
+								</section>
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner">
+
+							<!-- Search -->
+								<section id="search" class="alt">
+									<form method="post" action="#">
+										<input type="text" name="query" id="query" placeholder="아직 준비 안됌" />
+									</form>
+								</section>
+
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2>Menu</h2>
+									</header>
+									<ul>
+										<li><a href="login.php">Login</a></li>
+										<li><a href="register.html">Register</a></li>
+										<li><a href="element.html">Elements</a></li>
+										<li>
+											<span class="opener">Submenu</span>
+											<ul>
+												<li><a href="#">Ready for Order</a></li>
+												<li><a href="#">Ready for Order</a></li>
+												<li><a href="#">Ready for Order</a></li>
+												<li><a href="#">Ready for Order</a></li>
+											</ul>
+										</li>
+										<li><a href="board.php"><strong>Community Board</strong></a></li>
+										<li><a href="generic.html">Generic</a></li>
+										<li>
+											<span class="opener">Another Submenu</span>
+											<ul>
+												<li><a href="#">Ready for Order</a></li>
+												<li><a href="#">Ready for Order</a></li>
+												<li><a href="#">Ready for Order</a></li>
+												<li><a href="#">Ready for Order</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Maximus Erat</a></li>
+										<li><a href="#">Sapien Mauris</a></li>
+										<li><a href="#">Amet Lacinia</a></li>
+									</ul>
+								</nav>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>그냥 넣어보는 사진관</h2>
+									</header>
+									<div class="mini-posts">
+										<article>
+											<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+										</article>
+									</div>
+									<ul class="actions">
+										<li><a href="#" class="button">More</a></li>
+									</ul>
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Get in touch</h2>
+									</header>
+									<p>본 사이트는 대학생의 과제를 위해 임시로 제작된 사이트이며, 이미지는 과제 심사기간이 끝난 후 모두 내릴 예정입니다.</p>
+									<ul class="contact">
+										<li class="icon solid fa-envelope"><a href="#">shkim4548@sungkyul.ac.kr</a></li>
+										<li class="icon solid fa-phone">(000) 000-0000</li>
+										<li class="icon solid fa-home">성결대학교 미디어 소프트웨어 학과 학부생 김서현<br />
+										안양시 만안구 성결대학교</li>
+									</ul>
+								</section>
+
+							<!-- Footer -->
+								<footer id="footer">
+									<p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+								</footer>
+
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ registerAction.php
@@ -0,0 +1,43 @@
+﻿<html>
+<!-- <meta charset="utf-8"> -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+	<?php
+
+		$host = 'localhost';
+		$user = 'seohyun';
+		$pw = '1234';
+		$dbName = 'sungkyul';
+		$mysqli = new mysqli($host, $user, $pw, $dbName);
+
+		$member_email = $_POST['email'];
+	    $member_pw_1 = $_POST['pw_1'];
+	    $member_name = $_POST['name'];
+
+		$sql = "insert into Members (
+				member_email,
+				member_pw_1,
+				member_name
+		)";
+		
+		$sql = $sql. "values (
+				'$member_email',
+				'$member_pw_1',
+				'$member_name'
+		)";
+
+		if($mysqli->query($sql)){ 
+		  echo '<script>alert("success inserting")</script>'; 
+		}else{ 
+		  echo '<script>alert("fail to insert sql")</script>';
+		}
+
+		mysqli_close($mysqli);
+	  
+	?>
+
+<script>
+	location.href = "index.html";
+</script>
+
+</html>
